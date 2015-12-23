f00e074664aa768a8708d90cd28869bfd9506715
Cody Robinson <cody-admin@inzernettechnologies.com>
2015-12-23 01:44:40 -0500

    ADDED - Command argument catching

--- src/com/inzernettechnologies/bomblobbers/Command/main.java
+++ src/com/inzernettechnologies/bomblobbers/Command/main.java
@@ -11,20 +11,50 @@ import java.util.List;
 
 public class main implements CommandExecutor {
 
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-        if (label.equalsIgnoreCase("bl")){
-            if (args[0].equalsIgnoreCase("join")){
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (label.equalsIgnoreCase("bl")) {
+            if (args.length < 2){
+                sender.sendMessage("/bl <join|leave>");
+                return true;
+            }
+            if (args[0].equalsIgnoreCase("join")) {
+                if (args.length < 3){
+                    sender.sendMessage("/bl join <Blue|Read");
+                    return true;
+                }
                 team team = new team();
                 team.setTeam((Player) sender, com.inzernettechnologies.bomblobbers.enums.team.valueOf(args[1]));
                 sender.sendMessage("You joined the: " + com.inzernettechnologies.bomblobbers.enums.team.valueOf(args[1]) + " team");
-            } else if (args[0].equalsIgnoreCase("leave")){
+            } else if (args[0].equalsIgnoreCase("leave")) {
+
+                /*
+
+                team team = new team();
+                team.removeFromTeam((Player) sender);
+
+                TODO: Leave team
+
+                 */
+
             }
-        } else if (label.equalsIgnoreCase("bls")){
-            if (args[0].equalsIgnoreCase("create")){
+        } else if (label.equalsIgnoreCase("bls")) {
+            if (args.length < 2){
+                sender.sendMessage("/bls <create|addspawn>");
+                return true;
+            }
+            if (args[0].equalsIgnoreCase("create")) {
+                if (args.length < 3){
+                    sender.sendMessage("/bls create <map>");
+                    return true;
+                }
                 com.inzernettechnologies.bomblobbers.main.instance.getConfig().set("config.maps." + args[1] + ".enabled", false);
                 sender.sendMessage("Added map!");
                 com.inzernettechnologies.bomblobbers.main.instance.saveConfig();
-            } else if (args[0].equalsIgnoreCase("addspawn")){
+            } else if (args[0].equalsIgnoreCase("addspawn")) {
+                if (args.length < 4){
+                    sender.sendMessage("/bls addspawn <map> <Blue|Red>");
+                    return true;
+                }
                 List<String> spawns = com.inzernettechnologies.bomblobbers.main.instance.getConfig().getStringList("config.maps." + args[1] + "." + args[2].toLowerCase());
                 spawns.add(Bukkit.getPlayer(sender.getName()).getPlayer().getLocation().getWorld().getName() + "_" + Bukkit.getPlayer(sender.getName()).getPlayer().getLocation().getX() + "_" + Bukkit.getPlayer(sender.getName()).getPlayer().getLocation().getY() + "_" + Bukkit.getPlayer(sender.getName()).getPlayer().getLocation().getZ() + "_" + Bukkit.getPlayer(sender.getName()).getPlayer().getLocation().getYaw() + "_" + Bukkit.getPlayer(sender.getName()).getPlayer().getLocation().getPitch());
                 com.inzernettechnologies.bomblobbers.main.instance.getConfig().set("config.maps." + args[1] + "." + args[2].toLowerCase(), spawns);
@@ -32,11 +62,17 @@ public class main implements CommandExecutor {
                 // /bls addspawn <map[1]> <red|blue[2]>
                 com.inzernettechnologies.bomblobbers.main.instance.saveConfig();
             }
-        } else if (label.equalsIgnoreCase("bla")){
-            if (args[0].equalsIgnoreCase("regen")){
+        } else if (label.equalsIgnoreCase("bla")) {
+            if (args.length < 2){
+                sender.sendMessage("/bla <regen>");
+                return true;
+            }
+            if (args[0].equalsIgnoreCase("regen")) {
                 com.inzernettechnologies.bomblobbers.Regeneration.main regen = new com.inzernettechnologies.bomblobbers.Regeneration.main();
                 regen.regen();
             }
+        } else {
+            sender.sendMessage("Unknown command");
         }
         return true;
     }
